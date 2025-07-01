97bfb22ad0c8f2294f91ef60358135fc461d1829
Ashokkumar <152209063+Ashokkumar1574@users.noreply.github.com>
2025-07-01 21:20:32 +0530

    Template literals

---
+++ temp_literals.js
@@ -0,0 +1,14 @@
+const name = "Sam";
+const age = 25;
+
+// Basic usage
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+// Multiline strings
+const message = `Hello,
+This is a multiline
+string.`;
+console.log(message);
+
+// Expressions inside template literals
+console.log(`Next year, I will be ${age + 1}.`);
