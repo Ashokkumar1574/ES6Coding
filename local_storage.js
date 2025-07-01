aefbcd3e6750b151247957a377edf04a44c0dc98
Ashokkumar <152209063+Ashokkumar1574@users.noreply.github.com>
2025-07-01 22:16:41 +0530

    Local Storage

---
+++ local_storage.js
@@ -0,0 +1,12 @@
+//You use localStorage in the browser console or in JavaScript files that run in the browser (like in an HTML page).
+//It does not work in Node.js or the terminal, because they do not have a browser environment.
+
+
+//How to use,
+// Open your browser (Chrome, Edge, Firefox, etc.)
+// Press F12 to open Developer Tools.
+// Go to the Console tab.
+// Type or paste your localStorage code there."
+
+// localStorage.setItem('username', 'Alex');
+// console.log(localStorage.getItem('username')); // 'Alex'
